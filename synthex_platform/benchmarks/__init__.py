a98e41d89816d9fc8844b4fcdb0f240ac27b2e82
Olakitan Abdul-Malik Gbolahan <olakitangbolahan@gmail.com>
2026-09-15 15:57:08 +0100

    Expose Corrosion V1 benchmark scorer

--- synthex_platform/benchmarks/__init__.py
+++ synthex_platform/benchmarks/__init__.py
@@ -1,2 +1,14 @@
 from .catalog import all_benchmarks, benchmark_matrix
-__all__ = ["all_benchmarks", "benchmark_matrix"]
+from .corrosion import (
+    CorrosionBenchmarkScore,
+    CorrosionExpectedObservation,
+    score_corrosion_archive,
+)
+
+__all__ = [
+    "all_benchmarks",
+    "benchmark_matrix",
+    "CorrosionBenchmarkScore",
+    "CorrosionExpectedObservation",
+    "score_corrosion_archive",
+]
