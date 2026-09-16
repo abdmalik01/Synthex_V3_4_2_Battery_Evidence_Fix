e38b511e1a57c23820330fe059a2dde0f3a78f98
Olakitan Abdul-Malik Gbolahan <olakitangbolahan@gmail.com>
2026-09-16 13:22:54 +0100

    Export data-aware chart field discovery

--- synthex_platform/visual/analytics/__init__.py
+++ synthex_platform/visual/analytics/__init__.py
@@ -3,6 +3,14 @@
 from .exports import comparison_frame, export_visualization
 from .projection import project_archives
 from .models import AnalyticQuery, VisualizationSpec
-from .specs import build_visualization_spec
+from .specs import build_visualization_spec, chart_field_options
 
-__all__ = ["AnalyticQuery", "VisualizationSpec", "build_visualization_spec", "comparison_frame", "export_visualization", "project_archives"]
+__all__ = [
+    "AnalyticQuery",
+    "VisualizationSpec",
+    "build_visualization_spec",
+    "chart_field_options",
+    "comparison_frame",
+    "export_visualization",
+    "project_archives",
+]
